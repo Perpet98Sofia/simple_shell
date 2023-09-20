18636607435d694173a0b1698c30b08edbb7740c
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-09-20 15:30:28 -0700

    exit built-ins

---
+++ exit_builtin.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * built_in_exit - exit built-in
+ *
+ * @command: character used
+ * Return: 0 Success, -1 otherwise
+ */
+int built_in_exit(data_shell command)
+{
+	int status;
+
+	if (command.args[1])
+	{
+		status = atoi(command.args[1]);
+		if (status != 0)
+			exit(status);
+		else
+		{
+			command.status = 2;
+			get_error(command.args, command.status, command.counter);
+			return (-1);
+		}
+	}
+	else
+		exit(0);
+	return (0);
+}
