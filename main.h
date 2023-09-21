2588b22962dc886a0b434bf342637dee9fe44a1c
Yann Adjumany <yvay03@gmail.com>
2023-09-21 02:24:53 +0000

    ..

--- main.h
+++ main.h
@@ -153,7 +153,6 @@ char *_strncat(char *dest, char *src, int n);
 int execute(data_shell command);
 
 void free_all(char *strng, char *strg);
-void free_s(char *buf, char *buff, char *bufff, char *buffff);
 char *_strrchr(char *str, int c);
 int _strncmp(char *s1, char *s2, int n);
 int _strlen(char *s);
