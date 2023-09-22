b7c0be5f4f00d6a7d079a755676c317ff66b0296
Yann Adjumany <yvay03@gmail.com>
2023-09-22 10:31:35 +0000

    ..

--- _cd.c
+++ _cd.c
@@ -11,7 +11,7 @@ int cd_shell(data_shell command)
 	if (command.args[1] == NULL)
 		command.args[1] = _strdup("..");
 
-	cd_to(command, &command.status);
+	cd_to(&command);
 
 	return (command.status);
 }
@@ -24,23 +24,21 @@ int cd_shell(data_shell command)
  *
  * Return: no return
  */
-void cd_to(data_shell command, int *status)
+void cd_to(data_shell *command)
 {
-	char pwd[BUFFER_SIZE], dir[BUFFER_SIZE];
+	char *pwd, *dir;
 
 	getcwd(pwd, sizeof(pwd));
 
-	dir = command.args[1];
-	if (chdir(dir) == -1)
+	dir = command->args[1];
+	if (chdir(command->args[1]) == -1)
 	{
-		command.status = 2;
-		get_error(command.args, command.status, command.counter);
+		command->status = 2;
+		get_error(command->args, command->status, command->counter);
 		return;
 	}
 	getcwd(dir, BUFFER_SIZE);
 
-	update_value(command, "PWD", dir);
-	update_value(command, "OLDPWD", pwd);
-
-	*status = 0;
+	update_value(*command, "PWD", dir);
+	update_value(*command, "OLDPWD", pwd);
 }
